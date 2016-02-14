07df5e7847b6ede3b15b58f8397b87ef15ff424a
MusikPolice <jonfritz@gmail.com>
2016-02-13 20:24:31 -0500

    Changed registration of ManagedService instances to make sure that they are always singletons. This way, we know that the correct instance is stopped when the application closes

--- src/main/java/ca/jonathanfritz/budgey/ManagedService.java
+++ src/main/java/ca/jonathanfritz/budgey/ManagedService.java
@@ -1,8 +1,11 @@
 package ca.jonathanfritz.budgey;
 
+import com.google.inject.Singleton;
+
 /**
  * Objects that implement this interface will be stopped when the application stops
  */
+@Singleton
 public interface ManagedService {
 
 	void start();
--- src/main/java/ca/jonathanfritz/budgey/guice/BudgeyModule.java
+++ src/main/java/ca/jonathanfritz/budgey/guice/BudgeyModule.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import ca.jonathanfritz.budgey.ManagedService;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
 import com.google.inject.multibindings.Multibinder;
 
 /**
@@ -22,13 +23,14 @@ public class BudgeyModule extends AbstractModule {
 	protected void configure() {
 		// auto-discover and register managed services with classpath scanning
 		// this allows a list of all managed services to be injected as Set<ManagedService>
+		// each ManagedService instance is a singleton, so we know that the same one is used everywhere
 		log.debug("Registering managed services...");
 		final Reflections reflections = new Reflections("ca.jonathanfritz.budgey");
 		final Set<Class<? extends ManagedService>> services = reflections.getSubTypesOf(ManagedService.class);
 		final Multibinder<ManagedService> managedServiceBinder = Multibinder.newSetBinder(binder(), ManagedService.class);
 		for (final Class<? extends ManagedService> clazz : services) {
 			log.debug(clazz.getCanonicalName());
-			managedServiceBinder.addBinding().to(clazz);
+			managedServiceBinder.addBinding().to(clazz).in(Singleton.class);
 		}
 	}
 }
